06125f6182d616eb9669cea9dc4876d1b0171c57
arunlorenz <nsak1763@gmail.com>
2024-05-21 05:49:52 +0530

    update

--- app.py
+++ app.py
@@ -81,5 +81,9 @@ def student_portal():
 def bus_incharge_portal():
     return render_template('bus_incharge_portal.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
